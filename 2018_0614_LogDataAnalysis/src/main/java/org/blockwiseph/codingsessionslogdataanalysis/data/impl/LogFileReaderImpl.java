20a275cb36fd86e79c84190d61097132139db2d6
Hiten Vasnani <hvas89@gmail.com>
2018-06-14 08:40:50 -0700

    feature: implement reading from log file

---
+++ 2018_0614_LogDataAnalysis/src/main/java/org/blockwiseph/codingsessionslogdataanalysis/data/impl/LogFileReaderImpl.java
@@ -0,0 +1,49 @@
+package org.blockwiseph.codingsessionslogdataanalysis.data.impl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.blockwiseph.codingsessionslogdataanalysis.data.LogFileReader;
+import org.blockwiseph.codingsessionslogdataanalysis.logevent.EventFactory;
+import org.blockwiseph.codingsessionslogdataanalysis.logevent.InvalidLogLineException;
+import org.blockwiseph.codingsessionslogdataanalysis.logevent.LogEvent;
+
+import com.google.inject.Inject;
+
+public class LogFileReaderImpl implements LogFileReader {
+
+	private String filename;
+
+	@Inject
+	EventFactory eventFactory;
+
+	public LogFileReaderImpl(String filename) {
+		this.filename = filename;
+	}
+
+	public List<LogEvent> getEventsFromFile() {
+		List<LogEvent> eventsFromFile = new ArrayList<LogEvent>();
+		try {
+			Scanner sc = new Scanner(new File(filename));
+			readEventsFromScanner(sc, eventsFromFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return eventsFromFile;
+	}
+
+	private void readEventsFromScanner(Scanner sc, List<LogEvent> eventsFromFile) {
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			try {
+				LogEvent logEvent = eventFactory.fromLogLine(line);
+				eventsFromFile.add(logEvent);
+			} catch (InvalidLogLineException e) {
+			}
+		}
+	}
+
+}
--- 2018_0614_LogDataAnalysis/src/main/java/org/blockwiseph/codingsessionslogdataanalysis/di/LogEventsModule.java
+++ 2018_0614_LogDataAnalysis/src/main/java/org/blockwiseph/codingsessionslogdataanalysis/di/LogEventsModule.java
@@ -1,6 +1,11 @@
 package org.blockwiseph.codingsessionslogdataanalysis.di;
 
+import org.blockwiseph.codingsessionslogdataanalysis.data.LogFileReader;
+import org.blockwiseph.codingsessionslogdataanalysis.data.impl.LogFileReaderImpl;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 import lombok.AllArgsConstructor;
 
@@ -8,4 +13,10 @@ import lombok.AllArgsConstructor;
 public class LogEventsModule extends AbstractModule {
 	String inputFile;
 	String outputFile;
+
+	@Provides
+	@Singleton
+	public LogFileReader getFileReader() {
+		return new LogFileReaderImpl(inputFile);
+	}
 }
