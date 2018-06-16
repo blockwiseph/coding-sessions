1b6d765df45699fbeef50e4dfa90df19bb77533d
Michael Pacana <mbpacana@up.edu.ph>
2018-06-16 22:05:40 +0800

    [ADD] ReportOutputWriter

---
+++ 2018_0614_LogDataAnalysis/src/main/java/org/blockwiseph/codingsessionslogdataanalysis/data/impl/ReportOutputWriterImpl.java
@@ -0,0 +1,13 @@
+package org.blockwiseph.codingsessionslogdataanalysis.data.impl;
+
+import org.blockwiseph.codingsessionslogdataanalysis.data.ReportOutputWriter;
+import org.json.JSONObject;
+
+public class ReportOutputWriterImpl implements ReportOutputWriter{
+
+	@Override
+	public void writeToFile(JSONObject jsonObject) {
+
+	}
+
+}
